ad28c55c3f7529b498ee6048e55b6042a84cd754
Yahav Biran <yahavb@me.com>
2024-02-06 12:12:50 -0800

    pass s3 bucket as arg to docker

--- sd_hf_serve/app/compile-model.sh
+++ sd_hf_serve/app/compile-model.sh
@@ -7,5 +7,5 @@ if [ "$(uname -i)" = "x86_64" ]; then
   python /sd2_512_compile.py
 fi
 tar -czvf /${COMPILER_WORKDIR_ROOT}/${MODEL_FILE}.tar.gz /${COMPILER_WORKDIR_ROOT}/
-aws s3 cp /${COMPILER_WORKDIR_ROOT}/${MODEL_FILE}.tar.gz s3://${BUCKET}/${MODEL_FILE}_${DEVICE}.tar.gz
-aws s3api put-object-acl --bucket ${BUCKET} --key ${MODEL_FILE}_${DEVICE}.tar.gz --acl public-read
+aws s3 cp /${COMPILER_WORKDIR_ROOT}/${MODEL_FILE}.tar.gz s3://${BUCKET}/${MODEL_FILE}.tar.gz
+aws s3api put-object-acl --bucket ${BUCKET} --key ${MODEL_FILE}.tar.gz --acl public-read
