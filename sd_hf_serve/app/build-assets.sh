8c08a23bad3beaa770220dd7d6686ef53aaec0cd
Yahav Biran <yahavb@me.com>
2024-02-06 11:25:22 -0800

    pass s3 bucket as arg to docker

--- sd_hf_serve/app/build-assets.sh
+++ sd_hf_serve/app/build-assets.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
 ASSETS="-assets"
+BUCKET="sdinfer"
 export IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG$ASSETS
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $IMAGE
-docker build -t $IMAGE --build-arg ai_chip=$IMAGE_TAG  -f Dockerfile-assets .
+docker build -t $IMAGE --build-arg bucket=$BUCKET  -f Dockerfile-assets .
 docker push $IMAGE
