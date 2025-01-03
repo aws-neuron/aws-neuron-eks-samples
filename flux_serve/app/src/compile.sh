c4f3b8eaf6057f757ebfcf45da7cfd9844770a70
Yahav <yahavb@amazon.com>
2025-01-02 17:24:53 -0800

    compile

--- flux_serve/app/src/compile.sh
+++ flux_serve/app/src/compile.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-find $COMPILER_WORKDIR_ROOT -type d \( -name "compiled_model" -o -name "__pycache__" -o -name "compiler_workdir" \) -exec rm -rf {} +
+find . -type d \( -name "compiled_model" -o -name "__pycache__" -o -name "compiler_workdir" \) -exec rm -rf {} +
 
-myloc=$COMPILER_WORKDIR_ROOT
-python "$myloc/text_encoder_1/compile.py"
-python "$myloc/text_encoder_2/compile.py" -m 32
-python "$myloc/transformer/compile.py" -hh $HEIGHT -w $WIDTH -m 32
-python "$myloc/decoder/compile.py" -hh $HEIGHT -w $WIDTH
+python "./text_encoder_1/compile.py"
+python "./text_encoder_2/compile.py" -m 32
+python "./transformer/compile.py" -hh $HEIGHT -w $WIDTH -m 32
+python "./decoder/compile.py" -hh $HEIGHT -w $WIDTH
 echo "done compiling; cleaning compiler_workdir"
-find $COMPILER_WORKDIR_ROOT -type d \( -name "__pycache__" -o -name "compiler_workdir" \) -exec rm -rf {} +
+find . -type d \( -name "__pycache__" -o -name "compiler_workdir" \) -exec rm -rf {} +
