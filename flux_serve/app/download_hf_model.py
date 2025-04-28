8bec2fcfb2864ae58dcbed1c735c735ab928920f
Yahav <yahavb@amazon.com>
2025-04-27 21:33:35 -0700

    download_hf_model.py

--- flux_serve/app/download_hf_model.py
+++ flux_serve/app/download_hf_model.py
@@ -4,6 +4,6 @@ repo_id=os.environ['MODEL_ID']
 os.environ['NEURON_COMPILED_ARTIFACTS']=repo_id
 hf_token=os.environ['HUGGINGFACE_TOKEN'].strip()
 login(hf_token, add_to_git_credential=True)
-snapshot_download(repo_id=repo_id,local_dir=repo_id,token=hf_token)
+snapshot_download(repo_id=repo_id,local_dir="/"+repo_id,token=hf_token)
 print(f"Repository '{repo_id}' downloaded to '{repo_id}'.")
 
